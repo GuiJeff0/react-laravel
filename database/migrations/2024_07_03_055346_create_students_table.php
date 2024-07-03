4d5d36f5857f63575bc3561f7d5bbb6cc0ef27b9
creedx <guilhermejeffofc@gmail.com>
2024-07-03 03:08:00 -0300

    component Student react

--- database/migrations/2024_07_03_055346_create_students_table.php
+++ database/migrations/2024_07_03_055346_create_students_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('students', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->date('address');
+            $table->string('phone')->nullable();
             $table->timestamps();
         });
     }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use app\Http\Controllers\StudentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::apiResource('/student', StudentController::class);
